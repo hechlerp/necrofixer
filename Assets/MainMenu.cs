4ef138c6c4af4f2f291c6617ff56f6140e4b33c4
Charlie Harper <charlottehdesigns@gmail.com>
2020-02-02 14:25:36 -0500

    Music, scene, and effects Assets added in fmod prefabs (delete if things dont work) menu music and level Music music controller Script altered body controller Script Main menu visuals swapping body part animation and visual

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log ("Moving to the next scene");
     }
 
     public void QuitGame()
