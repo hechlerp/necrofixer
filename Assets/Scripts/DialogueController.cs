55d318bc819b08aea8a2384024a3107973c7f36f
Peter Hechler <peter.hechler@gmail.com>
2020-02-01 20:34:32 -0500

    add dialogue box for witch advisor

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Dictionary<string, int> dialogueTracker;
+    void Start()
+    {
+        Dialog.TextLoader.LoadDialog();
+        dialogueTracker = new Dictionary<string, int>() {
+            { "CustomerA_Prompt", 0 },
+            { "CustomerA_5", 1 },
+            { "CustomerA_3", 2 },
+            { "CustomerA_1", 3 },
+            { "CustomerB_Prompt", 4 },
+            { "CustomerB_5", 5 },
+            { "CustomerB_3", 6 },
+            { "CustomerB_1", 7 },
+            { "CustomerC_Prompt", 8 },
+            { "CustomerC_5", 9 },
+            { "CustomerC_3", 10 },
+            { "CustomerC_1", 11 }
+        };
+    }
+
+    public string getDialogueByName(string dialogueKey) {
+        return Dialog.TextLoader.GetDialog(dialogueTracker[dialogueKey]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/WitchAdvisorHUD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WitchAdvisorHUD : MonoBehaviour
+{
+    Scoring sc;
+    DialogueController dc;
+    CustomerManager cm;
+    private void Awake() {
+        sc = GameObject.Find("GlobalScripts").GetComponent<Scoring>();
+        dc = GameObject.Find("GlobalScripts").GetComponent<DialogueController>();
+        cm = GameObject.Find("GlobalScripts").GetComponent<CustomerManager>();
+    }
+
+    private void OnEnable() {
+        float roundScore = sc.getRoundScore();
+        float reviewScore = roundScore % 2 == 0 ? roundScore - 1 : roundScore;
+        string dialogueText = dc.getDialogueByName(cm.getCurrentCustomerName() + "_" + reviewScore);
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = dialogueText;
+    }
+
+    private void OnDisable() {
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+    }
+}
