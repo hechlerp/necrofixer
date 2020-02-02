38971806900c0fc7dc9fd04d3a1df1fe3e723877
Stongue <stongue87@gmail.com>
2020-02-01 19:15:52 -0500

    UI dialog box, demo scene setup, with example test code UI dialog needs a ID to show Text Textloader no longer a monobehavior but a static class

---
+++ Assets/Scripts/LoaderDialogTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderDialogTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dialog.TextLoader.LoadDialog();
+        FindObjectOfType<ToolUI.UI_Dialog>().DialogShow(2);
+    }
+
+  
+}
---
+++ Assets/Scripts/UI/UI_Dialog.cs
@@ -0,0 +1,84 @@
+﻿/* Text Loader
+ * load text in for Dialog
+ * 
+ * Scott Tongue
+ * 2020
+ */
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+namespace ToolUI
+{
+
+    [RequireComponent(typeof(Text))]
+    public class UI_Dialog : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Change fill in rate speed")]
+        [Range(0.01f, 0.1f)]
+        private float _fillInRate = 0.2f;
+        private Text _dialogbox;
+
+        #region Unity Calls
+        private void Awake()
+        {
+            _dialogbox = this.GetComponent<Text>();
+            Color color = _dialogbox.color;
+            color.a = 0f;
+            _dialogbox.color = color;
+        }
+        #endregion
+
+        #region public
+
+        /// <summary>
+        /// Toggle visablity of the text
+        /// </summary>
+        /// <param name="TurnOn">bool value for visablity</param>
+        public void ToggleDialogBox(bool TurnOn)
+        {
+            _dialogbox.enabled = TurnOn;
+        }
+        
+
+        /// <summary>
+        /// Skip the fade in and show it fully
+        /// </summary>
+        public void ShowFullText()
+        {
+            Color color = _dialogbox.color;
+            color.a = 255;
+            _dialogbox.color = color;
+            StopCoroutine(FadeTextIn());
+        }
+
+        /// <summary>
+        /// Show dialog
+        /// </summary>
+        /// <param name="ID">ID of text dialog to show</param>
+        public void DialogShow(int ID)
+        {
+            _dialogbox.text= Dialog.TextLoader.GetDialog(ID);
+            StartCoroutine(FadeTextIn());
+            
+        }
+
+        #endregion
+
+        #region Coroutines
+        IEnumerator FadeTextIn()
+        {
+
+            Color color = _dialogbox.color;
+            color.a = 0;
+            for (float i = 0; i <= 1f; i=+_fillInRate)
+            {
+                color.a+= _fillInRate;
+                _dialogbox.color = color;
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        #endregion
+    }
+}
