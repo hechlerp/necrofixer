4b881b1fc9b81e24dd36748d184ff8545de02645
Peter Hechler <peter.hechler@gmail.com>
2020-02-02 15:19:07 -0500

    add girl fieri prefab and scoring

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -24,15 +24,34 @@ public class Scoring : MonoBehaviour
             roundScores.Add(0f);
             return;
         }
+        Dictionary<string, float> consistency = new Dictionary<string, float>();
+        List<string> consistencyKeys = new List<string>();
         foreach (GameObject petPart in petParts) {
+
             BodyPartController bpc = petPart.GetComponent<BodyPartController>();
             string petPartName = bpc.GetComponent<SpriteRenderer>().sprite.name;
             string[] splitName = petPartName.Split('_');
             if (splitName[1] != "body") {
                 string animal = splitName[2];
                 string animalAlignment = alignments[animal];
-                if (animalAlignment == customerAlignment) {
+                if (customerAlignment == "CON") {
+                    float animalScore;
+                    if (consistency.TryGetValue(animal, out animalScore)) {
+                        consistency[animal] = consistency[animal] + 1;
+                    } else {
+                        consistency.Add(animal, 1f);
+                        consistencyKeys.Add(animal);
+                    }
+                }
+                else if(animalAlignment == customerAlignment) {
                     score++;
+                }
+            }
+        }
+        if (customerAlignment == "CON") {
+            foreach (string animalKey in consistencyKeys) {
+                if (consistency[animalKey] + 1 > score) {
+                    score = consistency[animalKey] + 1;
                 }
             }
         }
